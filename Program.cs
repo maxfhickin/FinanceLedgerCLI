509ff66bac0c2d6dff8e674346b55eb9249cae9a
Max <maxhickincf@gmail.com>
2026-09-20 19:51:08 +0100

    feat: added option 3 (view total), ammended option 2 to show number of entries and list them

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FinanceLedgerCLI
 {
@@ -6,34 +7,54 @@ namespace FinanceLedgerCLI
     {
         static void Main(string[] args)
         {
+            List<Transaction> transactions = new List<Transaction>(); // Creates a blank list to store the transactions
+
             while (true)
             {
                 Console.WriteLine("Menu: ");
                 Console.WriteLine("1. Add Transaction");
                 Console.WriteLine("2. View All");
-                Console.WriteLine("3. Exit");
+                Console.WriteLine("3. View Total");
+                Console.WriteLine("4. Exit");
                 Console.Write("Enter your choice here: ");
                 string choice = Console.ReadLine();
                 if (choice == "1")
                 {
-                    Console.WriteLine("You picked option 1");
+                    Console.WriteLine("\t\nYou picked option 1: Add Transaction\n");
                     Transaction transaction = new Transaction();
-                    Console.Write("Transaction Description: ");
+                    Console.Write($"\tTransaction Description: ");
                     transaction.Description = Console.ReadLine();
-                    Console.Write("Transaction Amount: ");
+                    Console.Write($"\tTransaction Amount: ");
                     transaction.Amount = decimal.Parse(Console.ReadLine());
                     transaction.Date = DateTime.Now;
-                    Console.WriteLine($"Transaction added: {transaction.Description}, £{transaction.Amount}, {transaction.Date}");
+                    Console.WriteLine($"\tTransaction added: {transaction.Description}, £{transaction.Amount}, {transaction.Date}\n");
+                    transactions.Add(transaction);
                 }
 
                 else if (choice == "2")
                 {
-                    Console.WriteLine("You picked option 2");
+                    Console.WriteLine("\n\tYou picked option 2\n");
+                    Console.WriteLine($"\tNumber of transactions: {transactions.Count}\n");
+                    foreach (Transaction t in transactions)
+                    {
+                        Console.WriteLine($"\tTransaction: {t.Description}, £{t.Amount}, {t.Date}");
+                    }
                 }
 
                 else if (choice == "3")
                 {
-                    Console.WriteLine("You picked option 3");
+                    Console.WriteLine("\n\tYou picked option 3\n");
+                    decimal total = 0;
+                    foreach (Transaction t in transactions)
+                    {
+                        total += t.Amount;
+                    }
+                    Console.WriteLine($"\tTotal: £{total}\n");
+                }
+
+                else if (choice == "4")
+                {
+                    Console.WriteLine("Goodbye!");
                     break;
                 }
                 
