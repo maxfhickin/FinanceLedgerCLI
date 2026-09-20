830c4a91ae8c9c9b4989d71256724b24f4c9a361
Max <maxhickincf@gmail.com>
2026-09-20 18:57:21 +0100

    feat: implement main menu loop

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FinanceLedgerCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1. Add Transaction");
+                Console.WriteLine("2. View All");
+                Console.WriteLine("3. Exit");
+                string choice = Console.ReadLine();
+                if (choice == "1")
+                {
+                    Console.WriteLine("You picked option 1");
+                }
+                if (choice == "2")
+                {
+                    Console.WriteLine("You picked option 2");
+                }
+                if (choice == "3")
+                {
+                    Console.WriteLine("You picked option 3");
+                    break;
+                }
+            }
+        }
+    }
+}
