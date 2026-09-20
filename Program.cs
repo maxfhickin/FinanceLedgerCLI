d1afeaa69d2666a6dee1ca9431fc3256c75b4961
Max <maxhickincf@gmail.com>
2026-09-20 19:13:15 +0100

    feat: add Transaction class with Description, Amount, and Date

--- Program.cs
+++ Program.cs
@@ -12,21 +12,42 @@ namespace FinanceLedgerCLI
                 Console.WriteLine("1. Add Transaction");
                 Console.WriteLine("2. View All");
                 Console.WriteLine("3. Exit");
+                Console.Write("Enter your choice here: ");
                 string choice = Console.ReadLine();
                 if (choice == "1")
                 {
                     Console.WriteLine("You picked option 1");
+                    Transaction transaction = new Transaction();
+                    Console.Write("Transaction Description: ");
+                    transaction.Description = Console.ReadLine();
+                    Console.Write("Transaction Amount: ");
+                    transaction.Amount = decimal.Parse(Console.ReadLine());
+                    transaction.Date = DateTime.Now;
+                    Console.WriteLine($"Transaction added: {transaction.Description}, £{transaction.Amount}, {transaction.Date}");
                 }
-                if (choice == "2")
+
+                else if (choice == "2")
                 {
                     Console.WriteLine("You picked option 2");
                 }
-                if (choice == "3")
+
+                else if (choice == "3")
                 {
                     Console.WriteLine("You picked option 3");
                     break;
                 }
+                
+                else
+                {
+                    Console.WriteLine("Invalid Choice. Please try again.");
+                }
             }
         }
+    }
+    public class Transaction
+    {
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
     }
 }
